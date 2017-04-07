6ff48bce2048a832dbccb1bf6a3253af422e7304
Jayasurian Makkoth <jayasurian123@yahoo.com>
2017-04-07 15:39:29 +0200

    validate halfmove clock

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 
-const validateSideToMove = (notation) => /w|b/.test(notation);
+const validateSideToMove = (notation) => /^(w|b)$/.test(notation);
 
 const validateCastlingAbility = (notation) => /^-$|^(KQ?k?q?|Qk?q?|kq?|q)$/.test(notation);
 
 const validateEnPassantTarget = (notation) => /^(-|[a-h][36])$/.test(notation);
 
+const validateHalfMoveClock = (notation) => /^([0-9]|[1-9][0-9])$/.test(notation);
+
 const parseFEN = (fen) => {
   const fenArr = fen.split(' ');
   const [piecePlacement, sideToMove,
          castlingAbility, enPassantTarget,
-         ...rest] = fenArr;
+         halfMoveClock, ...rest] = fenArr;
 
   return (fenArr.length === 6) &&
           validateSideToMove(fenArr[1]) &&
           validateCastlingAbility(castlingAbility) &&
-          validateEnPassantTarget(enPassantTarget);
+          validateEnPassantTarget(enPassantTarget) &&
+          validateHalfMoveClock(halfMoveClock);
 }
 
 
