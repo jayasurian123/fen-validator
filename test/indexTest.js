cc8cb150802c60bc343319c750a377c983e94b18
Jayasurian Makkoth <jayasurian123@yahoo.com>
2017-04-07 14:10:18 +0200

    added checking for en passant validation

--- test/indexTest.js
+++ test/indexTest.js
@@ -33,4 +33,51 @@ describe('Parsing FEN function', () => {
       expect(result).to.be.true;
     });
   });
+
+  context('checking en passant target square', () => {
+    let preString = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq';
+    let postString = '0 1';
+
+    it('checks the non en passant for both players', () => {
+      const notation = `${preString} - ${postString}`;
+      const result = parse(notation);
+      expect(result).to.be.true;
+    });
+
+    it('checks no more than 2 letters possible', () => {
+      const notation = `${preString} a11 ${postString}`;
+      const result = parse(notation);
+      expect(result).to.be.false;
+    });
+
+    it('checks for invalid combinations', () => {
+      const invalidComb = ['1a', 'i1', 'a2', 'h7', ''];
+      invalidComb.forEach((pair) => {
+        const notation = `${preString} ${pair} ${postString}`;
+        const result = parse(notation);
+        expect(result).to.be.false;
+      });
+
+    });
+    context('when has 2 letters', () => {
+      it('checks the first letter is from the range [a - h]', () => {
+        const validLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+        validLetters.forEach((letter) => {
+          const notation = `${preString} ${letter}3 ${postString}`;
+          const result = parse(notation);
+          expect(result).to.be.true;
+        });
+      });
+
+      it('checks the first letter is either 3 or 6', () => {
+        const validLetters = [3, 6];
+        validLetters.forEach((letter) => {
+          const notation = `${preString} a${letter} ${postString}`;
+          const result = parse(notation);
+          expect(result).to.be.true;
+        });
+      });
+    });
+
+  });
 });
